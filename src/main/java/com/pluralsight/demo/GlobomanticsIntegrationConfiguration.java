89b20cf142094ca0541e973161f258aa0a1c2653
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-08-07 14:36:50 -0300

    Added Java DSL configuration

---
+++ src/main/java/com/pluralsight/demo/GlobomanticsIntegrationConfiguration.java
@@ -0,0 +1,26 @@
+package com.pluralsight.demo;
+
+import com.pluralsight.demo.service.RegistrationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.messaging.MessageChannel;
+
+@Configuration
+public class GlobomanticsIntegrationConfiguration {
+
+    @Bean
+    public MessageChannel registrationRequest() {
+        return MessageChannels.direct("registrationRequest").get();
+    }
+
+    @Bean
+    public IntegrationFlow integrationFlow(RegistrationService registrationService) {
+        return IntegrationFlows.from("registrationRequest")
+                .handle(registrationService, "registrer")
+                .get();
+    }
+}
